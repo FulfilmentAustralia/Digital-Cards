ab53d6f25aff7441263deb642119eabccd4cb04a
FA <lillian@dts.com.au>
2026-07-23 13:15:42 +1000

    Add files via upload

---
+++ paul-woplin/sw.js
@@ -0,0 +1,19 @@
+const CACHE = "paul-woplin-card-v1";
+const ASSETS = ["./qr.html", "./index.html", "./icon-192.png", "./icon-512.png", "./paul-avatar.jpg"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).catch(() => {})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
